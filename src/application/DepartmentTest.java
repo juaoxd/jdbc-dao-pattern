e3b09aad8da5a1dba951c9fa24d4ba8452c1898f
João Gabriel <joaogabrielbochlivio@gmail.com>
2023-08-29 02:38:25 -0300

    feat: add department findAll method

--- src/application/DepartmentTest.java
+++ src/application/DepartmentTest.java
@@ -4,6 +4,8 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.util.List;
+
 public class DepartmentTest {
     public static void main(String[] args) {
         DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
@@ -11,5 +13,11 @@ public class DepartmentTest {
         System.out.println("=== TEST 1: department find by id ===");
         Department department = departmentDao.findById(2);
         System.out.println(department);
+
+        System.out.println("\n=== TEST 1: department findAll ===");
+        List<Department> list = departmentDao.findAll();
+        for (Department d : list) {
+            System.out.println(d);
+        }
     }
 }
